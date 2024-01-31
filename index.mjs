d90e280d53ba9f4b30cb3b0cfe33f76d339c5351
salino3 <programacionflavio@gmail.com>
2024-01-31 10:09:49 +0100

    commit

--- index.mjs
+++ index.mjs
@@ -1,28 +1,74 @@
 import http from 'http';
-import { getBookList } from './mock-db.mjs';
+import { deleteBook, getBook, getBookList, insertBook, updateBook } from './mock-db.mjs';
 
-// GET /api/books
+
 const handleRequest = (req, res) => {
-    const {url, method} = req;
-    if(url === "/api/books" && method === "GET"){
+  const { url, method } = req;
+  if (url === "/api/books") {
+    if (method === "GET") {
+      try {
         res.setHeader("Content-Type", "application/json");
         res.write(JSON.stringify(getBookList()));
         res.statusCode = 200;
         res.end();
-
-    } else if(/\/api\/books\/\d$/.test.test(url) && method === "GET"){
-        const [, bookId] = url.match(/\/api\/books\/\d$/);
-        res.setHeader("Content-Type", "application/json");
-        const book = getBook(Number(bookId));
-        res.write(JSON.stringify(book));
-        res.statusCode = 200;
+      } catch (error) {
+        res.statusCode = 500;
         res.end();
+      }
+    } else if (method === "POST") {
+        let data = [];
+        req.on("data", (chunk) => {
+            console.log({chunk});
+            data += chunk;
+        });
+
+        req.on("end", () => {
+
+            const book = JSON.parse(data.toString("utf-8"));
+            const newBook = insertBook(book);
+            res.setHeader("Content-Type","application/json");
+            res.statusCode = 201;
+            res.write(JSON.stringify(newBook));
+            res.end();
+        });
 
     }
-     else {
-        res.write("My awesome books portal");
-        res.end();
-    }
+  } else if (/\/api\/books\/\d$/.test(url) ) {
+
+      const [, bookIdString] = url.match(/\/api\/books\/(\d)$/);
+      const bookId = Number(bookIdString);
+   if(method === "GET") {
+
+       res.setHeader("Content-Type", "application/json");
+       const book = getBook(bookId);
+       res.write(JSON.stringify(book));
+       res.statusCode = 200;
+       res.end();
+   }  else if (method == "PUT") {
+  let data = [];
+  req.on("data", (chunk) => {
+    console.log({ chunk });
+    data += chunk;
+  });
+
+  req.on("end", () => {
+    const book = JSON.parse(data.toString("utf-8"));
+    updateBook(bookId, book);
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 204;
+    res.end();
+  });
+
+   } else if(method === "DELETE") {
+    deleteBook(bookId);
+    res.statusCode = 204;
+    res.end();
+   }
+
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 };
 
 const server = http.createServer(handleRequest);
